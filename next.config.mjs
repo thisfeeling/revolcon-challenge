838d3788b1bcc0f78ddba89194d54556a271351b
Stiven <89946683+thisfeeling@users.noreply.github.com>
2025-10-28 10:57:58 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
